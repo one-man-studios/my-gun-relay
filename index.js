f38f1655e947808ce7be6d4a0d92890ece2529ed
Sudarshan <87654111+one-man-studios@users.noreply.github.com>
2025-12-24 09:46:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,16 @@ const Gun = require('gun');
 const app = express();
 const port = process.env.PORT || 8080;
 
+// This allows your Neocities site to connect to this server
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
+
 app.use(Gun.serve);
 
-const server = app.listen(port, () => {
-  console.log('Relay peer listening on port', port);
+const server = app.listen(port, "0.0.0.0", () => {
+  console.log('Gun relay is running on port', port);
 });
 
 Gun({ web: server });
